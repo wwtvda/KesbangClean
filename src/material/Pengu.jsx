1de0dd787c5eb73754d5e62d9b0d37bb0ed5a905
septiyaTamara <septiyakristin98@mail.ugm.ac.id>
2019-08-06 06:12:01 +0700

    revi

--- src/material/Pengu.jsx
+++ src/material/Pengu.jsx
@@ -1,27 +1,48 @@
-import React, { Component } from "react";
-import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
-import p1 from './img/p1.jpg'
-import p2 from './img/p2.jpg'
-import p3 from './img/p3.jpg'
+import React, { Component } from 'react';
+import { MDBBtn, MDBCard, MDBContainer, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBRow, MDBCol } from 'mdbreact';
+import p1 from './img/p1.jpg';
+import p2 from './img/p2.jpg';
+import p3 from './img/p3.jpg';
 
-class Pengu extends Component {
+class CardExample extends Component {
   render() {
     return (
-      <MDBContainer>
-        <MDBRow>
-          <MDBCol lg="4" md="12" className="mb-3">
-          <img src={p1} className="img-fluid z-depth-1" alt="" />
-          </MDBCol>
-          <MDBCol lg="4" md="6" className="mb-3">
-            <img src={p2} className="img-fluid z-depth-1" alt="" />
-          </MDBCol>
-          <MDBCol lg="4" md="6" className="mb-3">
-            <img src={p3} className="img-fluid z-depth-1" alt="" />
-          </MDBCol>
+        <MDBContainer>
+      <MDBRow>
+        <MDBCol>
+          <MDBCard narrow>
+            <MDBCardImage cascade className="img-fluid" src={p2} />
+            <MDBCardBody cascade>
+              <MDBCardTitle>Pemenang Lomba</MDBCardTitle>
+              <MDBCardText>Pengumuman Pemenang Lomba Kreasi Poster Anti Narkoba 2018</MDBCardText>
+              <MDBBtn color="danger" href='/berita'>Selengkapnya</MDBBtn>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol>
+          <MDBCard cascade>
+            <MDBCardImage cascade className="img-fluid" src={p3} />
+            <MDBCardBody cascade>
+              <MDBCardTitle>Nominator Juara Lomba</MDBCardTitle>
+              <MDBCardText>Daftar Nominator Juara Lomba Kreasi Poster Anti Narkoba DIY 2018</MDBCardText>
+              <MDBBtn color="danger" href='/berita'>Selengkapnya</MDBBtn>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol>
+          <MDBCard wide>
+            <MDBCardImage cascade className="img-fluid" src={p1} />
+            <MDBCardBody cascade>
+              <MDBCardTitle>Lomba Poster</MDBCardTitle>
+              <MDBCardText>Lomba Kreasi Poster Anti Narkoba DIY 2018 untuk kalangan Pelajar SMA/SMK sederajat</MDBCardText>
+              <MDBBtn color="danger" href='/berita'>Selengkapnya</MDBBtn>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
       </MDBRow>
-    </MDBContainer>
-    );
+      </MDBContainer>
+    )
   }
 }
 
-export default Pengu;
+export default CardExample;
